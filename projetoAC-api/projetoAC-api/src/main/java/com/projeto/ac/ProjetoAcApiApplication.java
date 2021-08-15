1cd35e990e6ce99037ae884bb2731c628e461fe0
Pedro Junio <pedrofjduarte@gmail.com>
2021-08-15 14:20:20 -0300

    Criação do projeto 'projetoAC-api' com Spring Boot.

---
+++ projetoAC-api/projetoAC-api/src/main/java/com/projeto/ac/ProjetoAcApiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAcApiApplication.class, args);
+	}
+
+}
---
+++ projetoAC-api/projetoAC-api/src/test/java/com/projeto/ac/ProjetoAcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
